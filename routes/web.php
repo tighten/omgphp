4dfda82b1c036b77eea40053f18423639bb3bd86
Matt Stauffer <matt@tighten.co>
2019-08-09 22:21:24 -0400

    Drop login/register routes for now

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
+// Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
